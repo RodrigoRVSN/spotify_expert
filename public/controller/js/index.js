f29720e842023f6abdec6b22c727d6b90d8ed9f6
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-03-20 13:14:21 -0300

    ✨ feat: add presentation layer onload assignment

---
+++ public/controller/js/index.js
@@ -0,0 +1,7 @@
+import Controller from "./controller.js";
+import View from './view.js'
+
+console.log('oiioasdioasdioasdioasdioasdio')
+Controller.initialize({
+  view: new View()
+})
---
+++ public/controller/js/view.js
@@ -0,0 +1,16 @@
+export default class View {
+  onLoad() {
+    this.changeCommandBtnsVisibility()
+  }
+
+  changeCommandBtnsVisibility(hide = 'true') {
+    Array.from(document.querySelectorAll('[name=command]'))
+    .forEach(btn => {
+      const fn = hide ? 'add' : 'remove'
+      btn.classList[fn]('unassigned')
+      function onClickReset() {}
+      btn.onclick = onClickReset
+    })
+  }
+  
+}
