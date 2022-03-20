977ff600d56697a65c53294207aa02a31c39f0a8
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-03-20 17:04:12 -0300

    🚀 deploy: heroku commands

--- server/index.js
+++ server/index.js
@@ -3,3 +3,9 @@ import server from "./server.js";
 import { logger } from "./util.js";
 
 server.listen(config.port).on('listening', () => logger.info(`🔥 Server running in http://localhost:${config.port}`))
+
+// impede que caia a aplicacao caia com um erro não tratado
+// uncaughtException => Throw
+// unhandledRejection => Promises
+process.on('uncaughtException', (error) => logger.error(`uncaughtException happened: ${error.stack || error }`))
+process.on('unhandledRejection', (error) => logger.error(`unhandledRejection happened: ${error.stack || error }`))
